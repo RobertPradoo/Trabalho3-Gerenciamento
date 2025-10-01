b92232ffd4664fd7355e4f6b2b414b137bde1838
bgmartins <bgmartin@ucs.br>
2025-10-01 20:52:30 -0300

    feat(migrations): ✨ Adiciona migração inicial para o modelo Carro
    * Cria a classe de migração inicial para o modelo `Carro`
    * Define os campos `id` e `descricao`
    * Configura opções de metadados para o modelo
    * Registra o aplicativo `carros` nas configurações do Django

--- carros/models.py
+++ carros/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+from django.db import models
+
+
+class Carro(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricao = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "carro"
+        verbose_name = "Carro"
+        verbose_name_plural = "Carros"
