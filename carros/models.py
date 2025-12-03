4d1765d37757fc25c77114597f1f653a4816e009
Bruno Gonçalves Martins <bgmartins@ucs.br>
2025-12-03 17:26:43 -0300

    feat(migrations): ✨ Adiciona dados iniciais de carros, pilotos e corridas
    * Cria registros iniciais para os modelos `Carro`, `Piloto` e `Corrida`
    * Remove migrações desnecessárias
    * Corrige o nome do campo `safety_car` na classe `Corrida`

--- carros/models.py
+++ carros/models.py
@@ -26,6 +26,22 @@ class Piloto(models.Model):
         verbose_name_plural = "Pilotos"
 
 
+class Corrida(models.Model):
+    id = models.AutoField(primary_key=True)
+    local = models.CharField(max_length=100)
+    data = models.DateField()
+    tamanho_grid = models.IntegerField(
+        help_text="Número máximo de inscrições permitidas para a corrida."
+    )
+    safety_car = models.BooleanField(default=False)
+    count_batidas = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "corrida"
+        verbose_name = "Corrida"
+        verbose_name_plural = "Corridas"
+
+
 class InscricaoCorrida(models.Model):
     id = models.AutoField(primary_key=True)
     carro = models.ForeignKey(Carro, on_delete=models.CASCADE)
@@ -34,25 +50,11 @@ class InscricaoCorrida(models.Model):
     posicao_largada = models.IntegerField(null=True, blank=True)
     posicao_chegada = models.IntegerField(null=True, blank=True)
     tempo_corrida = models.DurationField(null=True, blank=True)
+    corrida = models.ForeignKey(
+        Corrida, on_delete=models.CASCADE, related_name="inscricoes"
+    )
 
     class Meta:
         db_table = "inscricao_corrida"
         verbose_name = "Inscrição Corrida"
         verbose_name_plural = "Inscrições Corrida"
-
-
-class Corrida(models.Model):
-    id = models.AutoField(primary_key=True)
-    local = models.CharField(max_length=100)
-    data = models.DateField()
-    tamanho_grid = models.IntegerField(
-        help_text="Número máximo de inscrições permitidas para a corrida."
-    )
-    safity_car = models.BooleanField(default=False)
-    count_batidas = models.IntegerField(default=0)
-    inscricoes = models.ManyToManyField(InscricaoCorrida, related_name="corridas")
-
-    class Meta:
-        db_table = "corrida"
-        verbose_name = "Corrida"
-        verbose_name_plural = "Corridas"
